df4f522c0268acb4100b77d5c24d27936a24cde2
najibbaraka <najib.baraka@epitech.eu>
2022-12-20 18:08:51 +0100

    update RTT EMPLOYEUR

--- src/components/PageJoursFeries/JoursFeries.js
+++ src/components/PageJoursFeries/JoursFeries.js
@@ -1,17 +1,13 @@
 import { useEffect, useState } from "react";
 import JFadmin from "./JFadmin";
-
 import JFuser from "./JFuser";
 import axios from "axios";
 import AddJF from "./AddJF";
 import cookie from 'react-cookies';
 
 export default function JoursFeries() {
-
     let resultAxios;
-
     const [jfs, setJfs] = useState([]);
-
     const urlBackend = "http://127.0.0.1:3001/";
 
     function fetchJF() {
@@ -30,7 +26,6 @@ export default function JoursFeries() {
 
     let type = selectedJour.type;
 
-
     const initSelected = {
         _id: "",
         date: "",
@@ -38,6 +33,7 @@ export default function JoursFeries() {
         jour: "",
         libelle: ""
     };
+
     const addJour = {
         _id: "",
         date: "",
@@ -45,13 +41,14 @@ export default function JoursFeries() {
         jour: "",
         libelle: ""
     }
-    function handleAdd(obj) {
 
+    function handleAdd(obj) {
         setSelectedJour(obj);
         //console.log(selectedJour);
-
     }
+
     //selectedJour.jour属性需要在这里计算
+
     function handleDate(e) {
         //console.log(e.target.value);
         selectedJour.date = e.target.value;
@@ -62,66 +59,46 @@ export default function JoursFeries() {
         //console.log(date);
         //console.log(selectedJour.jour);
     };
+
     function handleType(e) {
         //console.log(e.target.value);
         selectedJour.type = e.target.value;
     };
+
     function handLibelle(e) {
         //console.log(e.target.value);
         selectedJour.libelle = e.target.value;
     };
-    
+
     async function valitation() {
         const errorValider = document.querySelector("#errorValider");
-        errorValider.className="text-danger";
-        if (selectedJour.date ==="" || selectedJour.type ==="" || selectedJour.libelle ==="") {
-            
+        errorValider.className = "text-danger";
+        if (selectedJour.date === "" || selectedJour.type === "" || selectedJour.libelle === "") {
             errorValider.innerHTML = "Date Type et Motif sont obligatoires !";
-            
-        }else if (!selectedJour._id) {
+        } else if (selectedJour.type === "RTT Employeur" && checkJFW(selectedJour.date)) {
+            errorValider.innerHTML = checkJFW(selectedJour.date);
+        } else if (!selectedJour._id) {
             delete selectedJour._id;
-
             if (selectedJour.type === "Jour Férié") {
                 resultAxios = await axios.post(urlBackend + 'addjourferie', selectedJour).then((res) => {
                     setJfs(res.data);
                     return ("OK");
                 });
-
                 setSelectedJour(initSelected);
             } else if (parseInt(cookie.load("rttp")) > 0) {
-                    resultAxios = await axios.post(urlBackend + 'addjourferie', selectedJour).then((res) => {
-                        setJfs(res.data);
-                        return ("OK");
-                    });
-                    if (resultAxios === "OK") {
-                        resultAxios = await axios.post(urlBackend + 'updateRttp', { rttp: parseInt(cookie.load("rttp")), newRttp: (parseInt(cookie.load("rttp")) - 1) });
-                        cookie.save("rttp", parseInt(cookie.load("rttp")) - 1);
-                    }
-
-
-                    setSelectedJour(initSelected);
-                }else{
-                    errorValider.innerHTML="Il ne reste plus assez de RTT Employeur";
-                };
-
-            
-
+                resultAxios = await axios.post(urlBackend + 'addjourferie', selectedJour).then((res) => {
+                    setJfs(res.data);
+                    return ("OK");
+                });
+                if (resultAxios === "OK") {
+                    resultAxios = await axios.post(urlBackend + 'updateRttp', { rttp: parseInt(cookie.load("rttp")), newRttp: (parseInt(cookie.load("rttp")) - 1) });
+                    cookie.save("rttp", parseInt(cookie.load("rttp")) - 1);
+                }
+                setSelectedJour(initSelected);
+            } else {
+                errorValider.innerHTML = "Il ne reste plus assez de RTT Employeur";
+            };
         } else {
-            /* const newJour = {
-                _id: selectedJour._id
-            };
-            if (selectedJour.date) {
-                newJour.date = selectedJour.date;
-            };
-            if (selectedJour.type) {
-                newJour.type = selectedJour.type;
-            };
-            if (selectedJour.jour) {
-                newJour.jour = selectedJour.jour;
-            };
-            if (selectedJour.libelle) {
-                newJour.libelle = selectedJour.libelle;
-            }; */
             resultAxios = await axios.post(urlBackend + 'updatejourferie', selectedJour).then((res) => {
                 setJfs(res.data);
             });
@@ -135,30 +112,31 @@ export default function JoursFeries() {
                 }
             };
             setSelectedJour(initSelected);
-
-
         };
     };
+
     async function handleDelete(obj) {
         resultAxios = await axios.post(urlBackend + 'deletejourferie', obj).then((res) => {
             setJfs(res.data);
             return ("OK");
         });
         if (resultAxios === "OK" && obj.type === "RTT Employeur") {
-
             resultAxios = await axios.post(urlBackend + 'updateRttp', { rttp: parseInt(cookie.load("rttp")), newRttp: (parseInt(cookie.load("rttp")) + 1) });
             cookie.save("rttp", (parseInt(cookie.load("rttp")) + 1));
-
         }
     };
 
+    function checkJFW(checkDate) {
+        const week = new Date(checkDate).getDay();
+        const checkJF = jfs.filter(day => day.date === checkDate);
+        if (week === 0 || week === 6 || checkJF.length !== 0) {
+            return "Non week-ends fériés pour RTT employeur.";
+        }else return false;
+    };
 
     useEffect(() => { fetchJF() }, []);
-
-
     if (selectedJour.type !== "") {
         return (<AddJF valitation={() => valitation()} handleType={(e) => handleType(e)} handLibelle={(e) => handLibelle(e)} handleDate={(e) => handleDate(e)} handleAdd={() => handleAdd(initSelected)} selectedJour={selectedJour} />);
-
     } else if (cookie.load("role") === "user") {
         return (
             <JFuser joursferies={jfs} />
@@ -166,10 +144,6 @@ export default function JoursFeries() {
     } else {
         return (
             <JFadmin handleDelete={(obj) => handleDelete(obj)} addJour={addJour} handleAdd={(obj) => handleAdd(obj)} joursferies={jfs} />
-
         );
     }
-
-
 }
-
